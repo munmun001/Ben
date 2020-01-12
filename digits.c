1adb3bf4de5e94f939f6dcc665e18bb0285b7f9f
munmun001 <ro_free@yahoo.com.hk>
2020-01-12 16:45:02 +0800

    Update digits.c

--- digits.c
+++ digits.c
@@ -4,23 +4,22 @@
 
 int digPow(int n, int p) {
   if (n>0&&p>0){
-  int j=n;
-  int i=1, c1 = 0,c2=0;
-  for (int m = 10; m <= j; m*=10)i++;
-  for (j; 10<=j;j/=10)
-  {
-    int k = 0;
-    k = j%10;
-    c1=k;
-    i--;
-    for (int a = (i+p);a>1;a--)c1*=k;
+    int j=n;
+    int i=1, c1 = 0,c2=0;
+    for (int m = 10; m <= j; m*=10)i++;
+    for (j; 10<=j;j/=10) {
+      int k = 0;
+      k = j%10;
+      c1=k;
+      i--;
+      for (int a = (i+p);a>1;a--)c1*=k;
+      c2+=c1;
+    }
+    c1 = j;
+    for (int a = p;a>1;a--) c1*=j;
     c2+=c1;
-  }
-  c1 = j;
-  for (int a = p;a>1;a--) c1*=j;
-  c2+=c1;
-  i=c2/n;
-  if (i<=0)i = -1;
-  return i;
-}else return -1;
+    i=c2/n;
+    if (i<=0)i = -1;
+    return i;
+  }else return -1;
 }
